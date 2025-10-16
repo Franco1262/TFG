98a2d2db186ff3c79b75c38fdba9c0dcb91a1891
Franco1262 <Franco1274@hotmail.com>
2025-10-16 11:43:40 +0200

    First commit

---
+++ include/utilities.h
@@ -0,0 +1,37 @@
+#pragma once
+#define LILYGO_ESP32S3_CAM_SIM7080G  // Has PSRAM
+
+#if defined(LILYGO_ESP32S3_CAM_SIM7080G)
+
+//Pins for handling communication with RS485 module
+#define RS485_RXD 11
+#define RS485_TXD 14
+#define RS485_DIR1 13
+
+//Pins for handling communications between LoRa Module and ESP32
+#define LORA_SCK 9
+#define LORA_MISO 3
+#define LORA_MOSI 12
+#define LORA_SS 10
+#define LORA_DI0 35
+#define LORA_RST 0
+
+//Pins for handling SSDMMC communication
+#define SDMMC_CMD 39
+#define SDMMC_CLK 38
+#define SDMMC_DATA 40
+
+#ifdef I2C_SDA
+#undef I2C_SDA
+#endif
+
+#ifdef I2C_SCL
+#undef I2C_SCL
+#endif
+
+//Used to communicate with the PMU
+#define I2C_SDA                     (15)
+#define I2C_SCL                     (7)
+
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,153 @@
+#include <Arduino.h>
+#include <FS.h>
+#include <SD_MMC.h>
+#define XPOWERS_CHIP_AXP2101
+#include "XPowersLib.h"
+#include "utilities.h"
+
+#define SENSOR_READ_TIMEOUT 2000
+XPowersPMU   PMU;
+
+File file;
+
+//Struct to store data read from the sensor.
+struct DataSensor
+{
+    float temperature;
+    float humidity;
+    int16_t conductivity;
+    int16_t ph;
+    int16_t nitrogen;
+    int16_t phosphorus;
+    int16_t potassium;
+} dataReadSensor;
+
+void printSDFile(const char* path) 
+{
+  if (!SD_MMC.begin()) {
+    Serial.println("Error inicializando SD");
+    return;
+  }
+
+  File file = SD_MMC.open(path, FILE_READ);
+  if (!file) {
+    Serial.println("No se pudo abrir el archivo");
+    return;
+  }
+
+  Serial.printf("Contenido de %s:\n", path);
+
+  // Leer línea por línea
+  while (file.available()) {
+    String line = file.readStringUntil('\n');
+    Serial.println(line);
+  }
+
+  file.close();
+  Serial.println("--- Fin del archivo ---");
+}
+
+
+void setup() 
+{
+    Serial.begin(115200);
+    delay(5000);
+    if (!PMU.begin(Wire, AXP2101_SLAVE_ADDRESS, I2C_SDA, I2C_SCL))
+    {
+        while (1)
+        {
+            delay(5000);
+            Serial.println("Failed to initialize power.....");
+        }
+    }
+    else
+        Serial.printf("getID:0x%x\n", PMU.getChipID());
+
+    Serial2.begin(4800, SERIAL_8N1, RS485_RXD, RS485_TXD);
+    PMU.setDC5Voltage(3300); // RTC main power channel 2700~ 3400V
+    PMU.enableDC5();
+
+    PMU.setALDO3Voltage(3300);   // SD Card VDD 3300
+    PMU.enableALDO3();
+
+    SD_MMC.setPins(SDMMC_CLK, SDMMC_CMD, SDMMC_DATA);   // set sdcard pin use 1-bit mode
+    if (!SD_MMC.begin("/sdcard", true)) 
+    {
+        Serial.println("Card Mount Failed");
+        while (1) 
+            delay(1000);
+    }
+
+    //printSDFile("/sensor_data.jsonl");
+    file = SD_MMC.open("/sensor_data.jsonl", FILE_APPEND);
+    if (!file) 
+    {
+        Serial.println("Error abriendo archivo");
+        return;
+    }
+    pinMode(RS485_DIR1, OUTPUT);
+    delay(5000);
+}
+
+void decodeData(const uint8_t (&response)[19])
+{
+    dataReadSensor.temperature  = (((uint16_t)response[4] << 8) | (uint16_t)response[5]) / 10.0; //C
+    dataReadSensor.humidity     = (((uint16_t)response[6] << 8) | (uint16_t)response[7]) / 10.0; //%
+    dataReadSensor.conductivity = ((uint16_t)response[8]  << 8) | (uint16_t)response[9]; //1µS/cm
+    dataReadSensor.ph           = (((uint16_t)response[10] << 8) | (uint16_t)response[11]) / 10; //
+    dataReadSensor.nitrogen     = ((uint16_t)response[12] << 8) | (uint16_t)response[13]; //1mg/kg(mg/L)
+    dataReadSensor.phosphorus   = ((uint16_t)response[14] << 8) | (uint16_t)response[15]; //1mg/kg(mg/L)
+    dataReadSensor.potassium    = ((uint16_t)response[16] << 8) | (uint16_t)response[17]; //1mg/kg(mg/L)
+}
+
+void readSensor() 
+{
+    uint8_t request[8] = {0x01, 0x03, 0x00, 0x00, 0x00, 0x07, 0x04, 0x08};
+    uint8_t response[19];
+    int k = 0;
+
+    digitalWrite(RS485_DIR1, HIGH); // Transmitir
+    for(int i = 0; i < 8; i++) 
+        Serial2.write(request[i]);
+    Serial2.flush();
+    digitalWrite(RS485_DIR1, LOW); // Recibir
+
+    // Leer respuesta con timeout
+    unsigned long start = millis();
+    while((k < 19) && ((millis() - start) < SENSOR_READ_TIMEOUT)) 
+    {
+        if(Serial2.available())
+            response[k++] = Serial2.read();      
+    }
+
+    decodeData(response);
+}
+
+void writeIntoSDCard()
+{
+    static uint8_t fileLinesCounter = 0;
+    String jsonLine = "{";
+    jsonLine += "\"temperature\":" + String(dataReadSensor.temperature) + ",";
+    jsonLine += "\"humidity\":" + String(dataReadSensor.humidity) + ",";
+    jsonLine += "\"conductivity\":" + String(dataReadSensor.conductivity) + ",";
+    jsonLine += "\"ph\":" + String(dataReadSensor.ph) + ",";
+    jsonLine += "\"nitrogen\":" + String(dataReadSensor.nitrogen) + ",";
+    jsonLine += "\"phosphorus\":" + String(dataReadSensor.phosphorus) + ",";
+    jsonLine += "\"potassium\":" + String(dataReadSensor.potassium);
+    jsonLine += "}\n";
+
+    file.print(jsonLine);
+    fileLinesCounter++;
+    if(fileLinesCounter == 10)
+    {
+        file.flush();
+        fileLinesCounter = 0;
+    }
+}
+
+void loop() 
+{
+    readSensor();
+    writeIntoSDCard();
+    delay(2000);
+}
